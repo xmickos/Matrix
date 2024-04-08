e2d8d3c6c8acd24927611f1ddbfddf71e9c93f8c
Привет Непривет <xmickos@MacBook-Pro-Privet.local>
2024-04-09 00:41:37 +0300

    added copy & move constructors and assignments

--- matrix.cc
+++ matrix.cc
@@ -1,14 +1,28 @@
 #include "matrix.hpp"
 
+using namespace matrix;
 
 int main(int argc, char** argv){
 
-    // std::vector<int> a(2500, 42);
+    // g++ -std=c++17 -DDEBUG_ -fsanitize=address,bool,bounds matrix.cc -o matrix
 
+    Matrix<int> m1eye = Matrix<int>::eye(8);
+    Matrix<int> m2eye = Matrix<int>::eye(8);
 
-    Matrix<int> m = Matrix<int>::eye(8);
+    Matrix<int> m3 = Matrix<int>::Matrix_add(m1eye, m2eye);
 
-    m.print();
+    Matrix<int> m4(m3);
+    Matrix<int> m5 = m3;
+
+    const Matrix<int> m10 = Matrix<int>::eye(10);
+
+    m4 = std::move(m10);
+
+    m4.print();
+
+    m1eye.print();
+
+    std::cout << m1eye.cols() << std::endl;
 
     return 0;
 }
--- matrix.hpp
+++ matrix.hpp
@@ -3,97 +3,182 @@
 #include<cstdlib>
 #include<algorithm>
 
-template <typename T>       // T = {int, double, float}
-class Matrix{
-    private:
+namespace matrix {
 
-        T** data = nullptr;
-        int cols, rows;
+    template <typename T>       // T = {int, double, float}
+    class Matrix{
+        private:
 
-    public:
+            T** data = nullptr;
+            int cols_, rows_;
 
-        Matrix(int cols_, int rows_, T val = T{}) : cols(cols_), rows(rows_) {
-            data = new T*[cols];
-            for(int i = 0; i < cols; ++i){
-                data[i] = new T[rows];
-                std::fill_n(data[i], rows, val);
-            }
+        public:
 
-            #ifdef DEBUG_
-            std::cout << "Matrix ctor" << std::endl;
-            #endif
-        }
-
-        template <typename Iterator>
-        Matrix(int cols_, int rows_, Iterator it, Iterator et) : cols(cols_), rows(rows_) {
-            if(cols_ <= 0 || rows_ <= 0){
-                std::cout << "Matrix sizes must be positive integers." << std::endl;
-                abort();
-            }
-
-            if(std::distance(it, et) != cols_ * rows_){
-                std::cout << "Wrong sequence length." << std::endl;
-                #ifdef DEBUG_
-                std::cout << "Sequence length is " << std::distance(it, et) << std::endl;
-                #endif
-                abort();
-            }
-
-            data = new T*[cols];
-
-            for(int i = 0; i < cols; ++i){
-                data[i] = new T[rows];
-                std::copy(it, std::next(it, rows), data[i]);
-                it = std::next(it, rows);
-            }
-
-            #ifdef DEBUG_
-            std::cout << "Matrix ctor" << std::endl;
-            #endif
-        }
-
-        static Matrix eye(int sz_) {
-            if(sz_ <= 0){
-                std::cout << "Matrix size must be positive integer." << std::endl;
-                abort();
-            }
-
-            int* seq_ = new int[sz_ * sz_]{};
-
-            for(int i = 0; i < sz_; ++i){
-                seq_[ i * (sz_ + 1) ] = (T)1.;
-            }
-
-            Matrix<T> m(sz_, sz_, seq_, seq_ + sz_ * sz_);
-            delete[] seq_;
-
-            return m;
-        }
-
-
-        void print() const {
-            #ifdef DEBUG_
-            std::cout << "Matrix output" << std::endl;
-            #endif
-
-            for(int i = 0; i < cols; ++i){
-                for(int j = 0; j < rows; ++j){
-                    std::cout << (data[i])[j] << " ";
+            Matrix(int cls, int rws, T val = T{}) : cols_(cls), rows_(rws) {
+                data = new T*[cols_];
+                for(int i = 0; i < cols_; ++i){
+                    data[i] = new T[rows_];
+                    std::fill_n(data[i], rows_, val);
                 }
-                std::cout << std::endl;
+
+                #ifdef DEBUG_
+                std::cout << "Matrix base ctor" << std::endl;
+                #endif
             }
-        }
 
+            Matrix(const Matrix& rhs) : cols_(rhs.cols_), rows_(rhs.rows_) {
 
+                #ifdef DEBUG_
+                std::cout << "Copy ctor" << std::endl;
+                #endif
 
-        ~Matrix() {
-            for(int i = 0; i < cols; ++i){
-                delete [] data[i];
+                data = new T*[cols_];
+
+                for(int i = 0; i < rhs.cols_; ++i){
+                    data[i] = new T[rows_];
+                    std::copy(rhs.data[i], rhs.data[i] + rows_, data[i]);
+                }
             }
-            delete [] data;
 
-            #ifdef DEBUG_
-            std::cout << "Matrix dtor" << std::endl;
-            #endif
-        }
-};
+            Matrix& operator=(const Matrix& rhs) {
+                #ifdef DEBUG_
+                std::cout << "Copy assign" << std::endl;
+                #endif
+
+                if(this == &rhs) return *this;
+
+                for(int i = 0; i < cols_; ++i){
+                    delete [] data[i];
+                }
+                delete [] data;
+
+                cols_ = rhs.cols_;
+                rows_ = rhs.rows_;
+                data = new T*[cols_];
+
+                for(int i = 0; i < cols_; ++i){
+                    data[i] = new T[rows_];
+                    std::copy(rhs.data[i], rhs.data[i] + rows_, data[i]);
+                }
+
+                return *this;       // ?
+            }
+
+            Matrix& operator=(Matrix&& rhs){
+                #ifdef DEBUG_
+                std::cout << "Move assign" << std::endl;
+                #endif
+
+                if(&rhs == this) return *this;
+
+                cols_ = rhs.cols_;
+                rows_ = rhs.rows_;
+
+                for(int i = 0; i < cols_; ++i){
+                    std::swap(data[i], rhs.data[i]);
+                }
+
+                return *this;
+            }
+
+
+
+            template <typename Iterator>
+            Matrix(int cls, int rws, Iterator it, Iterator et) : cols_(cls), rows_(rws) {
+                if(cols_ <= 0 || rows_ <= 0){
+                    std::cout << "Matrix sizes must be positive integers." << std::endl;
+                    abort();
+                }
+
+                if(std::distance(it, et) != cols_ * rows_){
+                    std::cout << "Wrong sequence length." << std::endl;
+                    #ifdef DEBUG_
+                    std::cout << "Sequence length is " << std::distance(it, et) << std::endl;
+                    #endif
+                    abort();
+                }
+
+                data = new T*[cols_];
+
+                for(int i = 0; i < cols_; ++i){
+                    data[i] = new T[rows_];
+                    std::copy(it, std::next(it, rows_), data[i]);
+                    it = std::next(it, rows_);
+                }
+
+                #ifdef DEBUG_
+                std::cout << "Matrix iterator ctor" << std::endl;
+                #endif
+            }
+
+            static Matrix eye(int sz_) {
+                if(sz_ <= 0){
+                    std::cout << "Matrix size must be positive integer." << std::endl;
+                    abort();
+                }
+
+                int* seq_ = new int[sz_ * sz_]{};
+
+                for(int i = 0; i < sz_; ++i){
+                    seq_[ i * (sz_ + 1) ] = (T)1.;
+                }
+
+                Matrix<T> m(sz_, sz_, seq_, seq_ + sz_ * sz_);
+                delete[] seq_;
+
+                return m;
+            }
+
+            static Matrix Matrix_add(Matrix<T>& m1, Matrix<T>& m2){
+                if(m1.cols_ != m2.cols_ || m1.rows_ != m2.rows_){
+                    std::cout << "Matrices are incompatible." << std::endl;
+                    abort();
+                }
+
+                Matrix<T> m_out(m1.cols_, m1.rows_);
+
+                for(int i = 0; i < m1.cols_; ++i){
+                    for(int j = 0; j < m1.rows_; ++j){
+                        m_out.data[i][j] = m1.data[i][j] + m2.data[i][j];
+                    }
+                }
+
+                return m_out;
+            }
+
+
+            void print() const noexcept {
+                #ifdef DEBUG_
+                std::cout << "Matrix output" << std::endl;
+                #endif
+
+                for(int i = 0; i < cols_; ++i){
+                    for(int j = 0; j < rows_; ++j){
+                        std::cout << (data[i])[j] << " ";
+                    }
+                    std::cout << std::endl;
+                }
+            }
+
+            int cols() const noexcept {
+                return cols_;
+            }
+
+            int rows() const noexcept {
+                return rows_;
+            }
+
+            ~Matrix() {
+                for(int i = 0; i < cols_; ++i){
+                    delete [] data[i];
+                }
+                delete [] data;
+
+                #ifdef DEBUG_
+                std::cout << "Matrix dtor" << std::endl;
+                #endif
+            }
+    };
+
+
+}
